987496e3f68cf24d456565007ce7c44274c1ea01
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-31 14:00:13 -0700

    chore: enforce trusted types

--- src/modules/server/Server.ts
+++ src/modules/server/Server.ts
@@ -1,13 +1,34 @@
 import { Compiler } from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import { Application } from 'express';
+import express, { Application } from 'express';
 
 export default class Server {
+  private reports: any[] = [];
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   run(app: Application, server: WebpackDevServer, compiler: Compiler): void {
-    // app.get('/api/sh/build', async (req: any, resp: any) => {
-    //   const response = await build();
-    //   resp.json(response);
-    // });
+    app.use(express.json({ type: ['application/json', 'application/csp-report', 'application/reports+json'] }));
+
+    app.post('/csp-report', (req, res) => {
+      this.reports.push(req.body);
+      res.status(204).end();
+    });
+
+    app.get('/csp-reports', (req, res) => {
+      res.json(this.reports);
+    });
+
+    app.use((req, res, next) => {
+      const csp = "default-src 'self'; require-trusted-types-for 'script'; report-uri /csp-report";
+      const header = process.env.CSP_REPORT_ONLY === 'true'
+        ? 'Content-Security-Policy-Report-Only'
+        : 'Content-Security-Policy';
+      res.setHeader(header, csp);
+      next();
+    });
+  }
+
+  getReports(): any[] {
+    return this.reports;
   }
 }
